f4f6b018aff6255282bca823a7415e5c5803182d
Alfonso Farías <77316261+chronologie7@users.noreply.github.com>
2021-11-10 03:10:55 -0300

    initial file

---
+++ active_hosts.sh
@@ -0,0 +1,10 @@
+countHost=0;
+for num in {2..254}; do
+    ping -c 1 -w 1 192.168.1.$num;
+    if [ $? -eq 0 ];
+    then
+        let "countHost++"
+        echo "#$countHost: 192.168.1.$num" >> active_hosts.txt;
+        echo "host encontrado";
+    fi
+done
